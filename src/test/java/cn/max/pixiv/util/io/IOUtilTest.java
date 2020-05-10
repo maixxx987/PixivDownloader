e593c11318f4f55676c24c938ff943c9b46eed46
maixxx987 <maixx987@gmail.com>
2020-05-10 11:53:13 +0800

    Update IOUtilTest.java

--- src/test/java/cn/max/pixiv/util/io/IOUtilTest.java
+++ src/test/java/cn/max/pixiv/util/io/IOUtilTest.java
@@ -14,31 +14,14 @@ class IOUtilTest {
 
     @Test
     void testLoadPropertiesFile() throws IOException {
-
         IOUtil.loadProperties();
     }
 
     @Test
     void testSetPropertiesFile() throws IOException {
         IOUtil.loadProperties();
-//        Constant.properties = new Properties(System.getProperty("user.dir") + "\\download", "", -1);
         Constant.properties.setDownloadPath(System.getProperty("user.dir") + "\\download\\aaaa\\test\\test");
         Constant.properties.setProxy("127.1.1.1", 2222);
         IOUtil.writeProperties();
-    }
-
-    @Test
-    void testFile() throws IOException {
-        String filePath = System.getProperty("user.dir") + File.separator + "download" + File.separator + "test" ;
-        System.out.println(filePath);
-//        System.out.println(filePath.endsWith(File.separator));
-        if (!filePath.endsWith(File.separator)){
-            filePath += File.separator;
-        }
-
-        System.out.println(filePath);
-        System.out.println(filePath.endsWith(File.separator));
-//        Files.createDirectories(Path.of(System.getProperty("user.dir") + File.separator + "download" + File.separator + "test" + File.separator));
-
     }
 }
